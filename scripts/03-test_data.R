27faff7517fb41c5be82bf35aa85da6808b0bf40
Abdul Ahad Qureshi <ahad.qureshi@mail.utoronto.ca>
2024-04-19 22:12:28 -0400

    updated tests

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,32 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the analysis dataset
+# Author: Abdul Ahad Qureshi
+# Date: 18 April 2024 
+# Contact: ahad.qureshi@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+
+# Test 1: Check if 'interest_rates' is numeric
+test_interest_rates_class <- class(analysis_data$interest_rates) == "numeric"
+print(paste("Is 'interest_rates' numeric:", test_interest_rates_class))
+
+# Test 2: Check if 'consumer_debt' is numeric
+test_consumer_debt_class <- class(analysis_data$VALUE) == "numeric"
+print(paste("Is 'consumer_debt' numeric:", test_consumer_debt_class))
+
+# Test 3: Check the range of 'interest_rates'
+interest_rates_range <- range(analysis_data$interest_rates)
+print(paste("Range of 'interest_rates':", interest_rates_range[1], "to", interest_rates_range[2]))
+
+# Test 4: Check the range of 'consumer_debt'
+consumer_debt_range <- range(analysis_data$VALUE)
+print(paste("Range of 'consumer_debt':", consumer_debt_range[1], "to", consumer_debt_range[2]))
+
+
